93fae7170c4cd00eb13debc1b14eea1354034bb5
Em <126619895+itkibee@users.noreply.github.com>
2024-10-18 12:10:09 -0600

    Add files via upload

---
+++ cs3220_A4/cs3220_A4_src/agentsPrograms.py
@@ -0,0 +1,133 @@
+#How do we decide which node from the frontier to expand next?
+from nodeClass import Node
+from queue import PriorityQueue
+from collections import deque
+
+def BestFirstSearchAgentProgram(f=None):
+  #with BFS we choose a node, n, with minimum value of some evaluation function, f (n).
+    
+    def program(problem):
+
+      node = Node(problem.initial)
+      #print(node)
+      #print(node.state)
+      frontier = PriorityQueue()
+      frontier.put((1,node))
+      reached = {problem.initial:node}
+
+      while frontier:
+        node = frontier.get()[1]
+        print("The node {} is extracted from frontier:".format(node.state))
+
+        if problem.goal_test(node.state):
+          print("We have found our goal: {}".format (node.state))
+          return node
+
+        #reached.add(node.state)
+        for child in node.expand(problem):
+            if child.state not in reached or child.path_cost<reached[child.state].path_cost:
+                #print(child)
+                print("The child node {}.".format(child))
+                frontier.put((1,child))
+                reached.update({child.state:child})
+            
+      return None
+
+    return program
+
+def BestFirstSearchAgentProgramForShow(f=None):
+  #with BFS we choose a node, n, with minimum value of some evaluation function, f (n).
+    
+  def program(problem):
+      #print(111)
+      steps = 0
+      allNodeColors = []
+      nodeColors = {k : 'white' for k in problem.graph.nodes()}
+
+      node = Node(problem.initial)
+      nodeColors[node.state] = "yellow"
+      steps += 1
+      allNodeColors.append(dict(nodeColors))
+
+      #print(node.state)
+      frontier = PriorityQueue()
+      frontier.put((1,node))
+
+      nodeColors[node.state] = "orange"
+      steps += 1
+      allNodeColors.append(dict(nodeColors))
+
+
+
+      reached = {problem.initial:node}
+
+      while not frontier.empty():
+        node = frontier.get()[1]
+        print("The node {} is extracted from frontier:".format(node.state))
+        nodeColors[node.state] = "red"
+        steps += 1
+        allNodeColors.append(dict(nodeColors))
+        #print(node)
+
+        if problem.goal_test(node.state):
+          nodeColors[node.state] = "green"
+          steps += 1
+          allNodeColors.append(dict(nodeColors))
+          return (node,steps,allNodeColors)
+          
+
+        #reached.add(node.state)
+        #print(node.expand(problem))
+        for child in node.expand(problem):
+            #print(child)
+            if child.state not in reached or child.path_cost<reached[child.state].path_cost:
+                print("The child node {}.".format(child))
+                frontier.put((1,child))
+                nodeColors[child.state] = "orange"
+                steps += 1
+                allNodeColors.append(dict(nodeColors))
+
+                reached.update({child.state:child})
+
+        # modify the color of explored nodes to blue
+        nodeColors[node.state] = "blue"
+        steps += 1
+        allNodeColors.append(dict(nodeColors))
+      print("No solution...")
+            
+      return None,steps,allNodeColors
+
+  return program
+
+
+def BreadthFirstSearchGraph():
+  def program(problem):
+    node = Node(problem.initial)
+    #early goal test
+    if problem.goal_test(node.state):
+      print("We have found our goal: {}".format (node.state))
+      return node
+    
+    frontier = deque()#FIFO
+    frontier.append(node)
+    reached = set()
+    reached.add(node.state)
+    
+    while frontier:
+      print("Reached: {} ".format(reached))
+      print("Frontier: {} ".format(frontier))
+      node = frontier.popleft()#FIFO
+      print("The node {} is extracted from frontier:".format(node.state))
+      
+      for child in node.expand(problem):
+        print("The child node {}.".format(child))
+        if problem.goal_test(child.state):
+          print("We have found our goal: {}".format (child.state))
+          return child
+
+        if child.state not in reached:
+          reached.add(child.state)
+          frontier.append(child)
+    return None
+
+  return program
---
+++ cs3220_A4/cs3220_A4_src/mazeProblemSolvingAgentClass.py
@@ -0,0 +1,27 @@
+from problemSolvingAgentProgramClass import SimpleProblemSolvingAgentProgram
+from mazeProblemClass import MazeProblem
+
+class MazeProblemSolvingAgent(SimpleProblemSolvingAgentProgram):
+  def __init__(self, initial_state=None, dataGraph=None, goal=None):
+    super().__init__(initial_state)
+    self.dataGraph=dataGraph
+    self.goal=goal
+
+  def update_state(self, state, percept):
+    return percept
+
+  def formulate_goal(self, state):
+    if self.goal is not None:
+      return self.goal
+    else:
+      print("No goal! can't work!")
+      return None
+
+  #a description of the states and actions necessary to reach the goal
+  def formulate_problem(self, state, goal):
+    #instance of Vacuum ProblemClass
+    problem = MazeProblem(state,goal,self.dataGraph)
+    return problem  
+
+  def search(self, problem):
+    pass

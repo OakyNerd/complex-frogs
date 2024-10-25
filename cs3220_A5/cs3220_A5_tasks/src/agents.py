dade84d5689bca4e87143cbd726bf54dfb323be0
Em <emilimerchant@gmail.com>
2024-10-25 16:02:00 +0000

    Oct 25, 2024, 10:02 AM

--- cs3220_A5/cs3220_A5_tasks/src/agents.py
+++ cs3220_A5/cs3220_A5_tasks/src/agents.py
@@ -1,7 +1,7 @@
 import math
 from agentsPrograms import *
 from mazeProblemSolvingAgentProClass import MazeProblemSolvingAgentPro
-from mazeProblemSolvingAgentProDrawClass import MazeProblemSolvingAgentProDraw
+#from mazeProblemSolvingAgentProDrawClass import MazeProblemSolvingAgentProDraw
 
 def ProblemSolvingMazeAgentAStar(initState,WorldGraph,goalState):
     return MazeProblemSolvingAgentPro(initState,WorldGraph,goalState,A_StarSearchAgentProgram(math.dist))
@@ -10,8 +10,8 @@ def ProblemSolvingMazeAgentAStar(initState,WorldGraph,goalState):
 def ProblemSolvingMazeAgentBFS(initState,WorldGraph,goalState):
     return MazeProblemSolvingAgentPro(initState,WorldGraph,goalState,BestFirstSearchAgentProgram())
 
-def ProblemSolvingMazeAgentBFSShow(initState,WorldGraph,goalState):
-    return MazeProblemSolvingAgentProDraw(initState,WorldGraph,goalState,BestFirstSearchAgentProgramForShow())
+# def ProblemSolvingMazeAgentBFSShow(initState,WorldGraph,goalState):
+#     return MazeProblemSolvingAgentProDraw(initState,WorldGraph,goalState,BestFirstSearchAgentProgramForShow())
 
 
 def ProblemSolvingMazeAgentBREADTH_FS(initState,WorldGraph,goalState):
